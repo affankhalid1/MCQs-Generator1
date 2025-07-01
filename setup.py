833bb678eb92a2fe8d8f77d6b3d9bd42d22e84ea
affankhalid1 <affankhalid560@gmail.com>
2025-07-01 05:03:21 -0700

    file updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="mcqgenerator",
+    version="0.0.1",
+    author="affan khalid",
+    author_email= "affankhalid560@gmail.com",
+    install_requires = ["python-dotenv","streamlit","langchain","langchain-openai","langchain-groq","PyPDF"],
+    packages = find_packages()
+)
